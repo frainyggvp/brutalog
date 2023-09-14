6aad504fb8f010b0e42a553d69c0d82d3ad1ff5e
FrainyGGVP <113284520+frainyggvp@users.noreply.github.com>
2023-09-14 23:55:38 +0300

    Update main.py to v1.1

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import requests
+import requests, threading
 
 try:
 	url = input('Enter URL: ')
+	if url[0:8] == r"https://" or url[0:7] == r"http://":
+		pass
+	else:
+		url="https://"+url
 except:
 	print('Incorrect url')
 try:
@@ -9,11 +13,33 @@ try:
 except:
 	print('Incorrect wordlist')
 
-with open(wordlist) as wl:
-	for i in (wl):
-		if i[0]=='/':
-			print((url+i[:-1]), 'Code:', requests.get(url+'/'+i[:-1]).status_code)
-		else:
-			print((url+'/'+i[:-1]), 'Code:', requests.get(url+'/'+i[:-1]).status_code)
+def dir():
+	print(" - - - Starting - - - ")
+	try:
+		with open(wordlist) as wl:
+			for i in (wl):
+				try:
+					if i[0]=='/':
+						r = requests.get(url+'/'+i[:-1])
+						headers = r.headers
+						status_code = r.status_code
+						print("[*] ", url+i[:-1], 'Code:', status_code)
+						if status_code == 200:
+							print(" Conteng-length:", headers["Content-length"])
+					else:
+						r = requests.get(url+'/'+i[:-1])
+						headers = r.headers
+						status_code = r.status_code
+						print("[*] ", url+'/'+i[:-1], 'Code:', r.status_code)
+						if status_code == 200:
+							print(" Conteng-length:", headers["Content-length"])
+				except Exception as e:
+					print(f"[-] Error: {e}")
+	except Exception as er:
+		print(f"[-] Error: {er}")
+	print('- - - Stopping program - - - ')
 
-print('Stopping program')
+dir_thread = threading.Thread(target=dir)
+dir_thread.start()
+dir_thread.join()
+dir()
